70a3b218402a0bb9b548aad2b465084f64bf69dc
Yotam Gingold <yotam@yotamgingold.com>
2014-12-26 17:53:40 -0500

    average.py learned to accept glob filename input

--- average.py
+++ average.py
@@ -6,7 +6,7 @@ from imagestack import *
 import os, sys
 
 def usage():
-    print >> sys.stderr, 'Usage:', sys.argv[0], '[--tile pixel_rows/pixel_columns] [--convert L|RGB|RGBA (default: RGB)] [--size-mismatch skip|error|crop-upperleft (default: error)] path/to/image1 [path/to/image2 ... path/to/imageN] path/to/output'
+    print >> sys.stderr, 'Usage:', sys.argv[0], '[--tile pixel_rows/pixel_columns] [--convert L|RGB|RGBA (default: RGB)] [--size-mismatch skip|error|crop-upperleft (default: error)] [--glob] path/to/image1 [path/to/image2 ... path/to/imageN] path/to/output'
     sys.exit(-1)
 
 argv = list( sys.argv[1:] )
@@ -35,11 +35,23 @@ try:
 except IndexError: usage()
 except ValueError: pass
 
+glob_filenames = False
+try:
+    i = argv.index( '--glob' )
+    glob_filenames = True
+    del argv[ i ]
+except ValueError: pass
+
 if len( argv ) < 2:
     usage()
 
 filenames = argv[:-1]
 outname = argv[-1]
+
+if glob_filenames:
+    from glob import glob
+    from itertools import chain
+    filenames = list( chain( *[ glob( fname ) for fname in filenames ] ) )
 
 if os.path.exists( outname ):
     print >> sys.stderr, 'ERROR: Output file exists, will not clobber:', outname
